955784c2075ee1e06623d927a12f1ab4f0718b02
SantiagoM99 <88895769+SantiagoM99@users.noreply.github.com>
2023-05-20 19:37:15 -0500

    pepepepepepe

--- measurements/monitoring/settings.py
+++ measurements/monitoring/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'measurements_db',
         'USER': 'measurements_user',
         'PASSWORD': 'isis2503',
-        'HOST': '0.0.0.0',
+        'HOST': '10.128.0.3',
         'PORT': '5432',
     }
 }
